b2ddffd92d274817e8838b879356e7caee1a7587
Amin Malik <ammalik17@gmail.com>
2017-06-20 16:09:41 +0800

    Conflict resolved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake acts...'
+
+10.times do
+
+  act_seed = Act.new(
+    name: Faker::RockBand,
+    good_for: ["Parties", "Weddings", "Festivals"].sample,
+    description: Faker::Lorem.paragraph,
+    genre: ["Rock", "Rap", "Acoustic"].sample
+    )
+
+  act_seed.save!
+end
+
+puts 'Finished!'
+
