1157afe3a843e7c74e924b49d71e006354f28fb5
Kirsten Judd <kirstensophie@gmail.com>
2017-06-20 14:44:07 +0800

    Added dashboard view, updated index view and stylesheets

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.create
+    redirect_to
+  end
+
+  def act_params
+    params.require(:user).permit(:name)
+  end
+end
