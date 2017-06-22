c898ec4c1b8ca6d9bfcd082815feba5c942f1bb2
Hamilton Le <hamilton.m.le@gmail.com>
2017-06-22 12:49:50 +0800

    fixed schema to be able to assign a booking to users and acts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622043834) do
+ActiveRecord::Schema.define(version: 20170622044731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,17 +47,13 @@ ActiveRecord::Schema.define(version: 20170622043834) do
   create_table "bookings", force: :cascade do |t|
     t.datetime "time"
     t.string "address"
-    t.bigint "users_id"
-    t.bigint "acts_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "booking_status"
     t.bigint "user_id"
     t.bigint "act_id"
     t.index ["act_id"], name: "index_bookings_on_act_id"
-    t.index ["acts_id"], name: "index_bookings_on_acts_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
-    t.index ["users_id"], name: "index_bookings_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -82,7 +78,5 @@ ActiveRecord::Schema.define(version: 20170622043834) do
 
   add_foreign_key "acts", "users"
   add_foreign_key "bookings", "acts"
-  add_foreign_key "bookings", "acts", column: "acts_id"
   add_foreign_key "bookings", "users"
-  add_foreign_key "bookings", "users", column: "users_id"
 end
